4c81f0bef3c751721cdfc85b6902cc9c9c51abaa
Leonard Heister <Leonard@admins-MacBook-Pro-2.local>
2020-03-24 13:20:25 +0100

    add Insufficient Modularization and cypher q.

---
+++ src/main/java/de/feststelltaste/modularization/insufficient/Bloated.java
@@ -0,0 +1,84 @@
+package de.feststelltaste.modularization.insufficient;
+
+public class Bloated implements IBloated {
+
+    @Override
+    public void a() {
+
+    }
+
+    @Override
+    public void b() {
+
+    }
+
+    @Override
+    public void c() {
+
+    }
+
+    @Override
+    public void d() {
+
+    }
+
+    @Override
+    public void e() {
+
+    }
+
+    @Override
+    public void f() {
+
+    }
+
+    @Override
+    public void g() {
+
+    }
+
+    @Override
+    public void h() {
+
+    }
+
+    @Override
+    public void i() {
+
+    }
+
+    @Override
+    public void j() {
+
+    }
+
+    @Override
+    public void k() {
+
+    }
+
+    @Override
+    public void l() {
+
+    }
+
+    @Override
+    public void m() {
+
+    }
+
+    @Override
+    public void n() {
+
+    }
+
+    @Override
+    public void o() {
+
+    }
+
+    @Override
+    public void p() {
+
+    }
+}
---
+++ src/main/java/de/feststelltaste/modularization/insufficient/IBloated.java
@@ -0,0 +1,21 @@
+package de.feststelltaste.modularization.insufficient;
+
+public interface IBloated {
+
+    void a();
+    void b();
+    void c();
+    void d();
+    void e();
+    void f();
+    void g();
+    void h();
+    void i();
+    void j();
+    void k();
+    void l();
+    void m();
+    void n();
+    void o();
+    void p();
+}
